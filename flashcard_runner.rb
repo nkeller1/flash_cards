3a4afe37c70e5f26277df62559d9f469c69f6ccb
Nathan Keller <keller.nathan@gmail.com>
2019-11-21 12:38:26 -0700

    Refactor of cards.txt for better questions.

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -9,12 +9,7 @@ require './lib/cardgenerator'
 
   @round = Round.new(
   @deck = Deck.new(
-  @cards = CardGenerator.new('./data/cards.txt').cards
-  # @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography),
-  # @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM),
-  # @card_3 = Card.new("What planet is furthest from the sun?", "Uranus", :STEM),
-  # @card_4 = Card.new("What is the name of the country that encompasses more than 250 islands?", "Indonesia", :Geography)
-  ))
+  @cards = CardGenerator.new('./data/cards.txt').cards))
 
 
 def start
@@ -22,7 +17,6 @@ def start
   puts "Welcome! You're playing with #{@round.deck.count} cards."
   until @round.turns.count == 4
     puts "-" *50
-    # require "pry"; binding.pry
     puts "This is card number #{@round.turns.count + 1} out of 4."
     puts "Question: #{@round.current_card.question}"
     puts "-" *50
